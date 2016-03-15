ac37e204bcd6f0966d917fa3c4b5570645af0bdb
Oregu7 <jekkicool@yandex.ru>
2016-03-15 20:12:44 +0300

    groups and subjects

--- models/users.js
+++ models/users.js
@@ -1,11 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Group = require('./group');
+var Subject = require('./subject');
 
 var User = new Schema({
 	login: {type: String, required: true, lowercase: true, trim: true, unique: true},
 	password: {type: String, required: true},
 	name: {type: String, required:true},
-	lastname: {type: String, required: true}
+	lastname: {type: String, required: true},
+	role: {type: Number, required: true},
+	group: {type: Schema.ObjectId, required:false, ref:'Group'},
+	subjects: {type: Schema.ObjectId, required:false, ref: 'Subject'}
 });
 
 var UserModel = mongoose.model('User', User);
--- routes/auth.js
+++ routes/auth.js
@@ -3,18 +3,26 @@ var router = express.Router();
 var db = require('../models/db');
 var User = require('../models/users').User;
 var passwordHash = require('password-hash');
+var faker = require('faker')
 
 /* GET users listing. */
 router.get('/login/', function(req, res, next) {
-	var user = new User({
-		login: 'Oregu7',
-		password: passwordHash.generate('qwerty123'),
-		name: 'Oleg',
-		lastname: 'Chicherov'
-	});
+	for(i=0; i<=24; i++){
+		var user = new User({
+			login: faker.internet.email(),
+			password: passwordHash.generate('qwerty123'),
+			name: faker.name.firstName(),
+			lastname: faker.name.lastName(),
+			role: 1,
+			group: '56e81b49a0f51f680bf3c588'
+		});
 
-	user.save();
+		user.save();
 
+	}
+	
+
+	
 	res.render('index');
 });
 
@@ -23,9 +31,13 @@ router.post('/login/', function(req, res, next){
 		var login = req.body.login.toLowerCase();
 		var password = req.body.password;
 
-		User.findOne({login:login}, function(err, doc){
-			if(doc && passwordHash.verify(password, doc.password)){
-				res.send(doc);
+		User.findOne({login:login})
+			.populate('group')
+			.populate('subjects')
+			.exec(function(err, user){
+			if(user && passwordHash.verify(password, user.password)){
+				delete user.password;
+				res.send(user);
 			}else{
 				var err = new Error('Bad Login or Password');
 				err.status = 400;
