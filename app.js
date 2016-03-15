b39b8d8a0dfd7c9a911648d3ca55fbb13e7ccf21
Oregu7 <jekkicool@yandex.ru>
2016-03-15 20:30:30 +0300

    groups and subj

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 app.use('/', routes);
 app.use('/auth', auth);
 
