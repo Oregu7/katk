3b7375c70fb777276b4f4968ed03db142daac36d
Oregu7 <jekkicool@yandex.ru>
2016-03-16 23:14:22 +0300

    mark API

--- models/group.js
+++ models/group.js
@@ -5,7 +5,8 @@ var Specialization = require('./specialization')
 var Group = new Schema({
 	name: {type: String, required: true, unique: true},
 	specialization: {type: Schema.ObjectId, ref: 'Specialization', required: true},
-	course: {type: Number, required: true}
+	course: {type: Number, required: true},
+	subjects: [{type: Schema.ObjectId, ref: 'Subject', required: false}]
 });
 
 var GroupModel = mongoose.model('Group', Group);
--- routes/marks.js
+++ routes/marks.js
@@ -2,28 +2,84 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models/db');
 var Mark = require('../models/mark');
-var Lesson = require('../models/lesson');
 var Group = require('../models/group');
 
-router.get('/:subjectId', function(req, res, next){
-	var response = [];
-	Lesson.find({subject: req.params.subjectId}, function(err, lessons){
-		if (err){
-			res.send(err);
-		}
-
-		lessons.forEach(function(key, lesson){
-			Group.find({specialization: lesson.specialization, course: lesson.course}, function(err, groups){
-				if(err){
-					return null
-				}
-
-				response = response.concat(groups)
-			})
-		})
-	});
-
-	res.send(response);
+router.get('/', function(req, res, next){
+	res.send('Marks');
 })
+
+router.get('/subject/:subjectId/groups', function(req, res, next){	
+	Group.find({subjects: req.params.subjectId})
+		.select('-subjects -__v')
+		.populate('specialization', 'name -_id')
+		.exec(function(err, groups){
+		if(err){
+			var err = new Error('Groups or Subject Not Found');
+			err.status = 404;
+			next(err);
+		}else{
+			res.send(groups)
+		}
+	})
+})
+
+router.get('/subject/:subjectId/groups/:groupId', function(req, res, next)){
+	Mark.find({subject: req.params.subjectId})
+		.populate('student', '_id name lastname group')
+		.where('student.group', req.params.groupId)
+		.exec(function(err, marks){
+			if(err){
+				console.log(err)
+				res.status(400).send('Bad markId')
+			}else{
+				res.send(marks)
+			}
+		})
+}
+
+router.post('/addMark', function(req, res, next){
+	mark = new Mark({
+		subject: req.body.subject,
+		student: req.body.student,
+		mark: req.body.mark
+	})
+
+	mark.save(function(err, mark){
+		if (err){
+			console.log(err)
+		}else{
+			res.send(mark._id)
+		}
+	})
+})
+
+router.delete('/delMark/:markId', function(req,res,next){
+	Mark.remove({_id: req.params.markId}, function(err){
+		if(err){
+			res.status(400).send('Bad markId')
+		}else{
+			res.status(200).send('success')
+		}
+	})
+})
+
+router.put('/editMark', function(req, res, next){
+	Mark.update(
+	{_id: req.body.id},
+	{
+		student: req.body.student,
+		subject: req.body.subject,
+		mark: req.body.mark
+	},function(err, model){
+		if(err){
+			res.status(400).send('Bad markId')
+		}else{
+			res.status(200).send('success')
+		}
+	}
+	)
+})
+
+
 
 module.exports = router;
