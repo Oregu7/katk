80b91675982de5e05a91ee698953512efb2b57c9
Oregu7 <jekkicool@yandex.ru>
2016-03-17 17:53:47 +0300

    mark and docs api 3

--- routes/docs.js
+++ routes/docs.js
@@ -66,21 +66,16 @@ router.post('/', multipartyMiddleware ,function(req, res, next){
 	//res.send({success: 'save'})
 })
 
-router.get('/:file', function(req, res, next){
+router.get('/file/:file', function(req, res, next){
 	Document.findOne({_id: req.params.file}, function(err, doc){
 		if(err){
 			res.send('File is Does Not Exist!')
 		}
+
 		console.log(doc)
-		//var type = doc.type;
 		res.type(doc.type)
 		res.sendFile(doc.file)
 	})
-	
-})
-
-router.get('/output/:userId', function(req, res, next){
-	console.log(req.params)
 	
 })
 
